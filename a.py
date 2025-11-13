578570458e005afc4cd260698a16a04a7647316d
Jahn <vieirajahnlucas@gmail.com>
2025-11-13 19:44:22 -0300

    Patch #1
    adds random enemy, patches prints and gameplay feedback

--- a.py
+++ a.py
@@ -1,6 +1,9 @@
 ALL_SPECIALS = {'fireball':'20 damage', 'grace':'+10 health', 'sleep':"enemy can't act"}
 ELEMENT_REACTIONS = {'fire':['grass', 'ice'], 'grass':['rock']}
 
+def okCheck():
+    input('(press Enter)\n')
+
 class char:
     def __init__(self, name:str, element:str, maxHealth:int, dmg:int, res:int, specials:list):
         self.name = name
@@ -15,30 +18,45 @@ class char:
     def attack(self, other):
         if other.element in ELEMENT_REACTIONS[self.element]:
             print('BONUS ELEMENTAL DAMAGE!')
-            other.health -= self.dmg * 2
+            other.health -= (self.dmg - other.res) * 2
+            print(f'{self.name} attacked with {(self.dmg - other.res) * 2} damage!')
             if other.health < 0:
                 other.health = 0
         else:
             other.health -= self.dmg - other.res
+            print(f'{self.name} attacked with {self.dmg - other.res} damage!')
             if other.health < 0:
                 other.health = 0
-    
-    def die(self):
-        return print(f"{self.name} has died!")
     
     def use_special(self, special, other):
         from re import findall
         if special in ALL_SPECIALS.keys():
+            print(f'{self.name} used {special}')
             if 'damage' in ALL_SPECIALS[special]:
-                other.health -= int(findall(r'\d+', ALL_SPECIALS[special])[0])
+                damage = int(findall(r'\d+', ALL_SPECIALS[special])[0])
+                print(f'{special} does {damage} damage!')
+                other.health -= damage
             elif 'health' in ALL_SPECIALS[special]:
-                self.health += int(findall(r'\d+', ALL_SPECIALS[special])[0])
+                healing = int(findall(r'\d+', ALL_SPECIALS[special])[0])
+                print(f'heals for {healing} points!')
+                self.health += healing
+                if self.health > self.maxHealth:
+                    self.health = self.maxHealth
+
+def randomEnemy():
+    from random import choice, randint
+    specials = []
+    for i in range(randint(0,3)):
+        spell = choice(list(ALL_SPECIALS.keys()))
+        while spell in specials:
+            spell = choice(list(ALL_SPECIALS.keys()))
+        specials.append(spell)
+    return char('enemy', choice(list(ELEMENT_REACTIONS.keys())), randint(15, 120), randint(5, 30), randint(0, 5), specials)
 
 player = char('Shy', 'fire', 100, 10, 2, ['fireball'])
+enemy1 = randomEnemy()
 
-enemy1 = char('Enemy', 'grass', 40, 5, 1, ['heal'])
-
-def enemy_turn(enemy:char, player:char):
+def enemyAI(enemy:char, player:char):
     possibleActions = {
         'attack':2,
         'heal':1
@@ -56,15 +74,12 @@ def enemy_turn(enemy:char, player:char):
     
     match choosenAction:
         case 'attack':
-            print(f'enemy attacks for {enemy.dmg - player.res} damage !')
             enemy.attack(player)
         case 'heal':
-            print('enemy heals !')
-            enemy.use_special('heal', enemy)
+            enemy.use_special('grace', enemy)
     
 
 while player.health and enemy1.health > 0:
-    from time import sleep
     print(f'{player.name} : {player.element} || {enemy1.name} : {enemy1.element}')
     print(f'health: {player.health} || health: {enemy1.health} \n')
 
@@ -79,21 +94,22 @@ while player.health and enemy1.health > 0:
         match action:
             case 'attack':
                 player.attack(enemy1)
-                print(f'player attacked for {player.dmg - enemy1.res}')
-                sleep(0.5)
+                okCheck()
             case 'special':
+                print('specials list:')
                 for s in player.specials: print(f'{s} => {ALL_SPECIALS[s]}')
                 player.use_special(input('choose the special: '), enemy1)
-                sleep(0.5)
+                okCheck()
             case _:
                 print('invalid input !')
     
     print(f'{player.name} : {player.element} || {enemy1.name} : {enemy1.element}')
     print(f'health: {player.health} || health: {enemy1.health} \n')
 
-    print('ENEMY TURN !')
-    enemy_turn(enemy1, player)
-    sleep(1)
+    if enemy1.health <= 0:
+        print('ENEMY TURN !')
+        enemyAI(enemy1, player)
+    okCheck()
     print('----------------------------------------\n')
 
 print(f'{player.name} : {player.element} || {enemy1.name} : {enemy1.element}')
