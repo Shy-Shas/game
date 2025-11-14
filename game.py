a6deee216f89c102fdcdf54d52bdd99a95018d76
Jahn <vieirajahnlucas@gmail.com>
2025-11-14 15:07:06 -0300

    patch #4
    adds player class with level up system, adds world difficulty for enemy balancing.
    Noticed problem with spells (everyone shares de same pp) fix later

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 #imports area
 from random import choice, randint
+from os import system, name
 
 #consts area
 ALL_SPECIALS = {'fireball':{'effect':'10 damage + d10 (FIRE)', 'uses':2},
@@ -92,22 +93,65 @@ class char:
                 if self.health > self.maxHealth:
                     self.health = self.maxHealth
 
+class playerChar(char):
+    def __init__(self, name, element, maxHealth, dmg, res, specials):
+        super().__init__(name, element, maxHealth, dmg, res, specials)
+        self.Level = 1
+        self.XP = 0
+        self.XPnextLevel = 50
+
+    def levelUP(self):
+        print(f'{self.name} leveld to level {self.Level}')
+        self.XP = 0
+        XPnextLevel *= 1,1
+        self.Level += 1
+
+        self.maxHealth += 10
+        self.dmg += 5
+        self.res += 1
+
+        if len(self.specials) != len(list(ALL_SPECIALS.keys())):
+            newSpecial = choice(list(ALL_SPECIALS.keys()))
+            while newSpecial in self.specials:
+                newSpecial = choice(list(ALL_SPECIALS.keys()))
+            self.specials.append(newSpecial)
+        
+    
+    def gainXP(self, amount:int):
+        print(f'{self.name} gained {amount} XP!')
+        self.XP += amount
+
+        if self.XP >= self.XPnextLevel:
+            self.XP -= self.XPnextLevel
+            self.levelUP()
+        
+        print(f'current XP: {self.XP} || xp to next level: {self.XPnextLevel}')
 
 #functions area
 def okCheck():
     input('(press Enter)\n')
 
-def randomEnemy():
+def randomEnemy(dif):
     enemyTipe = choice(list(ELEMENT_REACTIONS.keys()))
     specials = []
 
-    for i in range(randint(0,3)):
+    if dif == 'easy':
+        difMod = 1
+    elif dif == 'medium':
+        difMod = 3
+    elif dif == 'hard':
+        difMod = 5
+    else:
+        difMod = 10
+
+    #caps number of spells enemy can have by the number of spells availible in gme
+    for i in range(min(randint(0, difMod), len(ALL_SPECIALS) - len(specials))):
         spell = choice(list(ALL_SPECIALS.keys()))
         while spell in specials and enemyTipe.upper() not in spell:
             spell = choice(list(ALL_SPECIALS.keys()))
         specials.append(spell)
     
-    return char('enemy', enemyTipe, randint(15, 121), randint(5, 31), randint(0, 6), specials)
+    return char('enemy', enemyTipe, randint(15, 20*difMod), randint(5, 7*difMod), randint(0, 3*difMod), specials)
 
 # def debugEnemy(enemy:char, allActions:dict, toughts:dict, action:str):
 #     print('ENEMY DEBUGGER !!!!!')
@@ -143,49 +187,85 @@ def enemyAI(enemy:char, player:char):
     
     #debugEnemy(enemy, possibleActions, thought, choosenAction)
 
+def setWorldDif(playerLvl):
+    if playerLvl == 1:
+        dif = 'easy'
+    elif playerLvl == 3:
+        dif = 'medium'
+    elif playerLvl == 5:
+        dif = 'hard'
+    elif playerLvl >= 7:
+        dif == 'impossible'
+    
+    return dif
 
 #-------------------------------main program running-------------------------------#
-player = char('Shy', 'fire', 100, 10, 2, ['fireball', 'firebolt'])
-enemy1 = randomEnemy()  
 
-while player.health and enemy1.health > 0:
+player = playerChar('Shy', 'fire', 100, 10, 2, [])
+
+difficulty = setWorldDif(player.Level)
+enemy1 = randomEnemy(difficulty)  
+
+replay = ''
+while replay != 'no' or 'n':
+    while player.health and enemy1.health > 0:
+        print(f'{player.name} : {player.element} || {enemy1.name} : {enemy1.element}')
+        print(f'health: {player.health} || health: {enemy1.health} \n')
+
+        moves = ['attack', 'special']
+        print("actions:")
+        for a in moves:
+            if a != 'special' or len(player.specials) > 0:
+                print(f"- {a}")
+
+        
+        while True:
+            action = input('what will you do: ')
+            match action:
+                case 'attack':
+                    player.attack(enemy1)
+                    okCheck()
+                    break
+                case 'special' if len(player.specials) > 0:
+                    print('specials list:')
+                    for s in player.specials:
+                        print(f"{s} - {ALL_SPECIALS[s]['effect']}:{ALL_SPECIALS[s]['uses']}PP")
+                    player.use_special(input('choose the special: '), enemy1)
+                    okCheck()
+                    break
+                case _:
+                    print('invalid input !')
+        
+        print('-----------')
+        print(f'{player.name} : {player.element} || {enemy1.name} : {enemy1.element}')
+        print(f'health: {player.health} || health: {enemy1.health} \n')
+
+        if enemy1.health > 0 and 'sleep' not in enemy1.status:
+            print('ENEMY TURN !')
+            enemyAI(enemy1, player)
+        okCheck()
+        print('----------------------------------------\n')
+
     print(f'{player.name} : {player.element} || {enemy1.name} : {enemy1.element}')
     print(f'health: {player.health} || health: {enemy1.health} \n')
 
-    ACTIONS = ['attack', 'special']
-    print("actions:")
-    for a in ACTIONS:
-        print(f"- {a}")
+    if player.health == 0:
+        print('player looses !')
+    else:
+        print('player wins !!!')
+
+        xp = (enemy1.maxHealth + enemy1.dmg + enemy1.res) / 10
+        player.gainXP(xp)
+
+    #-----------------------------#
+
+    replay = input('keep going (yes/no): ')
+    while replay != 'yes' and 'no' and 'y' and 'n':
+        replay = input('keep going (yes/no): ')
     
-    action = ''
-    while action not in ACTIONS:
-        action = input('what will you do: ')
-        match action:
-            case 'attack':
-                player.attack(enemy1)
-                okCheck()
-            case 'special':
-                print('specials list:')
-                for s in player.specials:
-                    print(f"{s} - {ALL_SPECIALS[s]['effect']}:{ALL_SPECIALS[s]['uses']}PP")
-                player.use_special(input('choose the special: '), enemy1)
-                okCheck()
-            case _:
-                print('invalid input !')
-    
-    print(f'{player.name} : {player.element} || {enemy1.name} : {enemy1.element}')
-    print(f'health: {player.health} || health: {enemy1.health} \n')
+    print('A NEW FOE APPEARD !!!')
+    player.health = player.maxHealth
+        
+    enemy1 = randomEnemy()
 
-    if enemy1.health > 0 and 'sleep' not in enemy1.status:
-        print('ENEMY TURN !')
-        enemyAI(enemy1, player)
-    okCheck()
-    print('----------------------------------------\n')
-
-print(f'{player.name} : {player.element} || {enemy1.name} : {enemy1.element}')
-print(f'health: {player.health} || health: {enemy1.health} \n')
-
-if player.health == 0:
-    print('player looses !')
-else:
-    print('player wins !!!')
+    system('cls' if name == 'nt' else 'clear')
